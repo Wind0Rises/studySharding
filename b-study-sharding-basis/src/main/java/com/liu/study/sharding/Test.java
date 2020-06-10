fb306eafda73e9b94b1a78a5afbe40bec52a48b3
Wind0Rises <1474673858@qq.com>
2020-06-10 18:24:13 +0800

    fff

---
+++ b-study-sharding-basis/src/main/java/com/liu/study/sharding/Test.java
@@ -0,0 +1,20 @@
+package com.liu.study.sharding;
+
+/**
+ * Sharding-sphere由Sharding-JDBC、Sharding-Proxy和Sharding-Sidecar（计划中）这3款相互独立的产品组成。
+ * 他们均提供标准化的【数据分片】、【分布式事务】和【数据库治理功能】
+ * @author Liuweian
+ * @version 1.0.0
+ * @createTime 2020/6/10 9:46
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println(1 << 0);
+        System.out.println(1 << 1);
+        System.out.println(1 << 2);
+        System.out.println(1 << 3);
+        System.out.println(1 << 4);
+    }
+
+}
